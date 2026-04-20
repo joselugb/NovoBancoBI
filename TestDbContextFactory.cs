615f8ef61a94ba8c225f5b56ae0eb3d0afe90a9b
Gavilanes B. Jose <gex_gavilanesjl@produbanco.com>
2026-04-20 14:24:47 -0500

    Estructura de TestCase basicos

---
+++ TestDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Persistencia;
+using Microsoft.EntityFrameworkCore;
+
+public static class TestDbContextFactory
+{
+    public static BancoDbContext Create()
+    {
+        var options = new DbContextOptionsBuilder<BancoDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        return new BancoDbContext(options);
+    }
+}
+``
---
+++ TestsCuentas.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using Domain.Enumeradores;
+using Domain.Entidades;
+using Domain.Excepciones;
+
+public class TestsCuentas
+{
+	[Fact]
+	public void DebitoCuandoBalanceEsInsuficiente()
+	{
+		var cuenta = new Cuenta
+		{
+			Estado = EstadosCuenta.ACTIVA
+		};
+
+        Assert.Throws<InsuficienteBalanceExcepcion>(() => cuenta.Debito(100));
+    }
+
+
+    [Fact]
+    public void DebitoCuandoCuentaEsBloqueada()
+    {
+        var cuenta = new Cuenta
+        {
+            Estado = EstadosCuenta.BLOQUEADA
+        };
+
+        Assert.Throws<CuentaInactivaExcepcion>(() => cuenta.Debito(100));
+    }
+}
